ad01e2d08264f6285a669eded5cd3866d0019872
Яна Александровна Галаган <s0160132@edu.kubsu.ru>
2023-04-13 20:18:38 +0300

    Fix program

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,6 +5,6 @@ int main() {
   std::string name;
   std::cout << "Введите ваше имя: ";
   std::cin >> name;
-  std::cout << "Hello world from @" << name << std::endl;
+  std::cout << "Hello world from @" << name << "\n";
   return 0;
 }
