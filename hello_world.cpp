5a45c87e605010ed39c20702e7f95f4c4459fff2
Яна Александровна Галаган <s0160132@edu.kubsu.ru>
2023-04-13 20:22:48 +0300

    Добавлен комментарий

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,6 +5,7 @@ int main() {
   std::string name;
   std::cout << "Введите ваше имя: ";
   std::cin >> name;
+  //считываем имя пользователя
   std::cout << "Hello world from @" << name << "\n";
   return 0;
 }
